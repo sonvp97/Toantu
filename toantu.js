ae451eb752092a3d1a0691f00be3e1ba1c4395ab
sonvp97 <112803870+sonvp97@users.noreply.github.com>
2022-09-09 10:56:44 +0700

    first

---
+++ toantu.js
@@ -0,0 +1,8 @@
+var inputheght,inputwidtd;
+    inputheght = prompt("Nhập chiều cao");
+inputwidtd = prompt("Nhập chiều dài");
+
+Heght = parseInt(inputheght);
+Width = parseInt(inputwidtd);
+ area = Heght*Width;
+ document.write('Diện tích hình chữ nhật là'+area);
